3706c3571dd75eb5f2987b8c5a5507f6626a8d2d
Jegan <72399182+jegansriragavan@users.noreply.github.com>
2021-07-08 22:07:46 +0530

    Update intro.py
    Removed additional space and comment added for Loop

--- Python-Dicts/intro.py
+++ Python-Dicts/intro.py
@@ -1,5 +1,4 @@
-
 student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
 
-for key, value in student.items():
-    print(key, value)
+for key, value in student.items():                # Loop through both keys and values, by using the items() method
+    print(key, value)                               
